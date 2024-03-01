3534e60d29b49f7c104898758cb5e5dce7668422
AldebaraFork <eduardoamorim758@proton.me>
2024-03-01 15:52:54 -0300

    TESTE

--- ExerciciosOOP/OOP1/Program.cs
+++ ExerciciosOOP/OOP1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OOP1
+{
+
+    //Classe principal do programa, Aqui é considerado o ponto de entrada do programa
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class pessoa
+    {
+         private string nome;
+         private int idade;
+         private char sexo;
+         private int cpf;
+    }
+}
