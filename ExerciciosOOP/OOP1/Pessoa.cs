5638ff05a9e9842a21348b2bf24e5c53150a61e9
AldebaraFork <eduardoamorim758@proton.me>
2024-03-01 17:50:54 -0300

    Exercicio de classe e atributos

---
+++ ExerciciosOOP/OOP1/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace OOP1
+{
+     class Programa
+    {
+        static void Main(string[] args)
+        {
+
+            //Precisa botar o new para INSTANCIAR a variavel, que cria na memoria um OBJETO composto pelos atributos da classe
+            //Agora Dados é um objeto da classe Pessoa
+            Pessoa Dados = new Pessoa();
+
+            Console.WriteLine("Olá, Por favor digite seu nome: ");
+            Dados.Nome = Console.ReadLine();
+            //Aqui é um exemplo de como usar os atributos da classe
+            Dados.Idade = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+
+
+    internal class Pessoa
+    {
+        //Isso é um atributo public (significa que os atributos podem ser usados em outro arquivo)
+        public int Idade;
+        public string Nome;
+        public double CPF;
+        public string Genero;
+        public string Endereco;
+        public string Telefone;
+
+
+        
+    }
+}
