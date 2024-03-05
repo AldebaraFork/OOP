dc5d52a0f80d26e797d6d095d9bc5ea59b2c94ac
AldebaraFork <eduardoamorim758@proton.me>
2024-03-04 21:53:57 -0300

    Exercicio de calculo de um retangulo utilizando metodos

---
+++ ExerciciosOOP/Exercicio5OOP/Retangulo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5OOP
+{
+    internal class Retangulo
+    {
+        static void Main(string[] args)
+        {
+           CalculoRetangulo ResultadoRetangulo = new CalculoRetangulo();
+           
+
+            Console.WriteLine("Digite a largura do retangulo: ");
+            ResultadoRetangulo.largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do retangulo: ");
+            ResultadoRetangulo.altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("AREA = " + ResultadoRetangulo.Area());
+
+            Console.WriteLine("PERIMETRO = " + ResultadoRetangulo.Perimetro());
+
+            Console.WriteLine("DIAGONAL = " + ResultadoRetangulo.Diagonal());
+
+
+        }
+    }
+}
+
+
+public class CalculoRetangulo
+{
+    public double largura;
+    public double altura;
+
+    public double Area()
+    {
+        return largura * altura;
+    }
+    public double Perimetro()
+    {
+        return 2 * (largura + altura);
+    }
+
+    public double Diagonal()
+    {
+        return Math.Sqrt(largura * largura + altura * altura);
+    }
+}
