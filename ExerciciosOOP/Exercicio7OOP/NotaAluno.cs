dc95708bcfad6fafd718f3c6223713d0cb5fc1e8
AldebaraFork <eduardoamorim758@proton.me>
2024-03-05 20:45:46 -0300

    Exercicio de calculo de nota de aluno

---
+++ ExerciciosOOP/Exercicio7OOP/NotaAluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio7OOP
+{
+    internal class NotaAluno
+    {
+        static void Main(string[] args)
+        {
+
+           Aluno DadosAluno = new Aluno();
+
+            Console.Write($"Nome do Aluno:  {DadosAluno.nome} ");
+            DadosAluno.nome = Console.ReadLine();
+            Console.WriteLine("Digite as tres notas do aluno: ");
+            DadosAluno.nota1 = double.Parse(Console.ReadLine());
+            DadosAluno.nota2 = double.Parse(Console.ReadLine());
+            DadosAluno.nota3 = double.Parse(Console.ReadLine());
+
+            DadosAluno.NotaFinal = DadosAluno.nota1 + DadosAluno.nota2 + DadosAluno.nota3;
+
+            if (DadosAluno.NotaFinal > 60)
+            {
+                Console.WriteLine($"NOTA FINAL = {DadosAluno.NotaFinal}");
+                Console.WriteLine("APROVADO");
+            }else
+            {
+                Console.WriteLine($"NOTA FINAL = {DadosAluno.NotaFinal}");
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine($"FALTARAM {60 - DadosAluno.NotaFinal} PONTOS");
+            }
+
+        }
+    }
+
+    public class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        public double NotaFinal;
+
+
+    }
+
+}
