0a0da9814c2652982fa437d75b5c21d7a0e57d11
AldebaraFork <eduardoamorim758@proton.me>
2024-03-05 21:42:02 -0300

    Exercicio de calculo de circunferencia e raio SEM membros estaticos

---
+++ ExerciciosOOP/Exercicio8OOP/SemMembrosEstaticos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio8OOP
+{
+    internal class SemMembrosEstaticos
+    {
+        static void Main(string[] args)
+        {
+            Circuferencia Dados = new Circuferencia();
+            Console.Write("Entre o valor do raio: ");
+            Dados.ValorRaio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Circunferência: {Dados.CalculoCircunferenia().ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {Dados.CalculoVolume().ToString("F2",CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine($"Valor de PI: {Dados.Pi}");
+        }
+    }
+
+    public class Circuferencia
+    {
+
+        public double ValorRaio;
+        public double Pi = 3.14;
+
+
+        public double CalculoCircunferenia()
+        {
+            return 2 * Pi * ValorRaio;
+
+        }
+
+        public double CalculoVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(ValorRaio, 3);
+        }
+
+    }
+}
