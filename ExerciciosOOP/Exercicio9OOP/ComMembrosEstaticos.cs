87ee46f4a8d5cff9cb495dcc959d0e95d7719eda
AldebaraFork <eduardoamorim758@proton.me>
2024-03-06 16:15:42 -0300

    Vendo membros estaticos e suas aplicações

---
+++ ExerciciosOOP/Exercicio9OOP/ComMembrosEstaticos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio9OOP
+{
+    internal class ComMembrosEstaticos
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
