d10a58a87406ea73d6bc506f44ea4f6b1374cd30
AldebaraFork <eduardoamorim758@proton.me>
2024-03-06 16:53:00 -0300

    Exercicio de cotação de dolar utilizando membros estaticos

---
+++ ExerciciosOOP/Exercicio10OOP/Calculos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio10OOP
+{
+    //membros estaticos são membros que pertencem a classe e não a instancia do objeto
+    //ou seja nao é necessario CRIAR um objeto no programa principal para poder utilizar os membros estaticos de uma classe
+
+    internal class Calculos
+    {
+
+
+        public static double CotacaoDolar;
+        public static double IOF = 6;
+        public static double Dolares;
+        public static double ValorEmReais;
+
+        public static double ConversorDeMoeda()
+        {
+            return (CotacaoDolar * Dolares) + (CotacaoDolar * Dolares * IOF / 100);
+        }
+    }
+}
---
+++ ExerciciosOOP/Exercicio10OOP/MembrosEstaticos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio10OOP
+{
+    internal class MembrosEstaticos
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dolar HOJE: ");
+            Calculos.CotacaoDolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar: ");
+            Calculos.Dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Como mostrar apenas as duas casas decimais no conversor de moeda, é necessario instanciar a classe CultureInfo e usar a identação 
+
+            Console.WriteLine("Valor a ser pago em reais: " + Calculos.ConversorDeMoeda().ToString("F2", CultureInfo.InvariantCulture));
+
+          
+        }
+    }
+}
