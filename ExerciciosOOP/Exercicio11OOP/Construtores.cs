43b7f4b9993207f333edd3934382c7e9999ea9cf
AldebaraFork <eduardoamorim758@proton.me>
2024-03-06 21:31:36 -0300

    Exercicio utilizando construtores e sobrecarga

---
+++ ExerciciosOOP/Exercicio11OOP/Construtores.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11OOP
+{
+    internal class Construtores
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o valor do produto: ");
+            double valor = double.Parse(Console.ReadLine());
+         
+
+            //instanciando o objeto com os valores passados 
+
+            program Produto = new program(nome, valor);
+
+            Console.WriteLine(Produto);
+
+            Console.WriteLine("Digite o numero de produtos a ser adicionado ao estoque: ");
+            Produto.ProdutosAdicionados = int.Parse(Console.ReadLine());
+            Produto.QuantidadeProduto += Produto.ProdutosAdicionados;
+
+            Console.WriteLine($"Dados do produto atualizados: {Produto.NomeProduto}, R$ {Produto.PrecoProduto}, {Produto.QuantidadeProduto} unidades, Total: {Produto.ValorTotalEmEstoque}");
+
+
+
+            Console.WriteLine("Digite o numero de produto a ser removido do estoque: ");
+            Produto.ProdutosRemovidos = int.Parse(Console.ReadLine());
+            Produto.QuantidadeProduto -= Produto.ProdutosRemovidos;
+
+
+            Console.WriteLine($"Dados do produto atualizados: {Produto.NomeProduto}, R$ {Produto.PrecoProduto}, {Produto.QuantidadeProduto} unidades, Total: {Produto.ValorTotalEmEstoque}");
+
+        }
+
+    }
+
+
+}
+
