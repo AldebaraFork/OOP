7e6c5541765eea2f5c3b58ab6629569a39a48a3d
AldebaraFork <eduardoamorim758@proton.me>
2024-03-08 21:29:23 -0300

    Bem parecido com o exercicio 4 mas com construtores, metodos e estrutura condicional

---
+++ ExerciciosOOP/Exercicio12OOP/Estoque.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12OOP
+{
+    internal class Estoque
+    {
+
+        public string NomeUsuario;
+        public string NomeProduto;
+        public int QuantidadeProduto;
+        public double ValorProduto;
+
+
+        //Nesse construtor, o usuário pode passar os valores para os atributos da classe
+        //Ou seja, o usuário pode passar o nome do produto, a quantidade e o valor
+        //os parametros dentro do construtor começam com letra minuscula e o atributo com letra maiuscula
+        public Estoque(string nomeUsuario, string nomeProduto, int quantidadeProduto, double valorProduto)
+        {
+            NomeUsuario = nomeUsuario;
+            NomeProduto = nomeProduto;
+            QuantidadeProduto = quantidadeProduto;
+            ValorProduto = valorProduto;
+        }
+
+
+        public int AdicionarProduto(int QuantidadeProduto)
+        {
+            return QuantidadeProduto += QuantidadeProduto;
+        }
+
+        public int RemoverProduto(int QuantidadeProduto)
+        {
+            return QuantidadeProduto -= QuantidadeProduto;
+        }
+
+        //criando um método para calcular o valor total do estoque
+
+        public double ValorTotalEstoque()
+        {
+            return QuantidadeProduto * ValorProduto;
+        }
+
+
+    }
+}
---
+++ ExerciciosOOP/Exercicio12OOP/Loja2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12OOP
+{
+    internal class Loja2
+    {
+        static void Main(string[] args)
+        {
+            //Criando um objeto da classe Estoque e passando nenhum valor para os atributos
+            Estoque Loja = new Estoque("","",0,0.0);
+            Console.WriteLine("Por-favor digite seu nome: ");
+            Loja.NomeUsuario = Console.ReadLine();
+            Console.WriteLine("Olá " + Loja.NomeUsuario + " seja bem vindo a nossa loja!");
+
+            Console.WriteLine("Digite o nome do produto: ");
+            Loja.NomeProduto = Console.ReadLine();
+            Console.WriteLine("OK, agora digite a quantidade do produto: ");
+            Loja.QuantidadeProduto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora digite o valor do produto: ");
+            Loja.ValorProduto = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Deseja adicionar mais produtos ao estoque? S/N");
+            
+            string resposta = Console.ReadLine();
+
+            if (resposta == "S")
+            {
+                Console.WriteLine("Digite a quantidade que deseja adicionar: ");
+                int quantidade = int.Parse(Console.ReadLine());
+                Loja.AdicionarProduto(quantidade);
+                Console.WriteLine("A quantidade de produtos no estoque é: " + Loja.QuantidadeProduto);
+            }
+            else
+            {
+                Console.WriteLine("Deseja remover produtos do estoque? S/N");
+                resposta = Console.ReadLine();
+                if (resposta == "S")
+                {
+                    Console.WriteLine("Digite a quantidade que deseja remover: ");
+                    int quantidade = int.Parse(Console.ReadLine());
+                    Loja.RemoverProduto(quantidade);
+                    Console.WriteLine("A quantidade de produtos no estoque é: " + Loja.QuantidadeProduto);
+                }
+                else
+                {
+                    Console.WriteLine("O valor total do estoque é: " + Loja.ValorTotalEstoque());
+                }
+            }
+
+
+
+
+
+
+       
+
+        }
+    }
+}
