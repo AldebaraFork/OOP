d85708a73287e4e39291836515e6cfbbf42dfb8b
AldebaraFork <eduardoamorim758@proton.me>
2024-03-13 21:03:42 -0300

    Encapsulamento simples

--- ExerciciosOOP/Exercicio13OOP/Encapsulamento.cs
+++ ExerciciosOOP/Exercicio13OOP/Encapsulamento.cs
@@ -17,47 +17,51 @@ namespace Exercicio13OOP
 
         public string NomeUsuario;
         public string NomeRecebedor;
-
         public string ChavePix;
-        public double SaldoEmConta;
+        //exemplo de encapsulamento, os atributos são privados e só podem ser acessados por metodos publicos
+        private double _saldoEmConta;
         public double ValorTransferencia;
         public double SaldoFinal;
-        public string Senha1;
-        public string Senha2;
+        //atributos privados para a senha começam com underline para diferenciar dos atributos publicos
+        private string _senha1;
+        private string _senha2;
 
 
+        // metodo para realizar transferencia bancaria sem poluir o codigo principal
+        // com detalhes de implementação, requer apenas a chamada do metodo para realizar a transferencia 
+        //ainda precisa de ajustes que serão feitos em breve, como banco de dados e validações de dados 
         public void Transferencia()
         {
-              Console.WriteLine("Olá digite o nome de Usuario: ");
+            Console.WriteLine("Olá digite o nome de Usuario: ");
             NomeUsuario = Console.ReadLine();
             Console.WriteLine("Digite o saldo em conta: ");
-            SaldoEmConta = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            _saldoEmConta = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.WriteLine("Digite a senha: ");
-            Senha1 = Console.ReadLine();
+            _senha1 = Console.ReadLine();
             Console.WriteLine("Digite a senha novamente: ");
-            Senha2 = Console.ReadLine();
-            if (Senha1 != Senha2)
+            _senha2 = Console.ReadLine();
+            if (_senha1 != _senha2)
             {
                 Console.WriteLine("Senha incorreta");
                 Console.WriteLine("Digite a senha novamente: ");
-                Senha2 = Console.ReadLine();
+                _senha2 = Console.ReadLine();
             }
-            else if (Senha1 == Senha2)
+            else if (_senha1 == _senha2)
             {
                 Console.WriteLine("Digite a Chave Pix: ");
                 ChavePix = Console.ReadLine();
                 Console.WriteLine("Digite o nome do recebedor: ");
-               NomeRecebedor = Console.ReadLine();
+                NomeRecebedor = Console.ReadLine();
                 Console.WriteLine("Digite o valor da transferência: ");
                 ValorTransferencia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                if (ValorTransferencia > SaldoEmConta)
+                if (ValorTransferencia > _saldoEmConta)
                 {
                     Console.WriteLine("Saldo insuficiente para realizar a transferencia! ");
                 }
-                else if (ValorTransferencia <= SaldoEmConta)
+                else if (ValorTransferencia <= _saldoEmConta)
                 {
                     Console.WriteLine("Transferencia para: " + NomeRecebedor + " Concluida!");
-                    SaldoFinal = SaldoEmConta - ValorTransferencia;
+                    SaldoFinal = _saldoEmConta - ValorTransferencia;
 
                     Console.WriteLine("Saldo em conta: " + SaldoFinal);
 
