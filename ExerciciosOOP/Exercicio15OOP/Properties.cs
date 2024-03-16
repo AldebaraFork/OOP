f8c1450fca923658183c82da8772f0046aae15bf
AldebaraFork <eduardoamorim758@proton.me>
2024-03-15 21:51:11 -0300

    Properties e auto properties

---
+++ ExerciciosOOP/Exercicio15OOP/Properties.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio15OOP
+{
+    internal class Properties
+    {
+        private string _nome;
+
+
+        //propertie para o campo nome 
+        public string Nome{ 
+
+            //get serve para pegar o valor de um atributo
+            get { }
+            //set serve para alterar o valor desse atributo
+            set { } 
+
+            }
+    }
+}
